251fa38bf0a897b0350dba440d049b43de8d5ab8
joelrubio06 <joelrubio@iesmontsia.org>
2026-02-03 15:28:22 +0100

    feat: actualizar navegación del Sidebar con nuevos elementos y eliminar icono innecesario

--- sprint4-blink/src/components/layout/Sidebar.vue
+++ sprint4-blink/src/components/layout/Sidebar.vue
@@ -72,9 +72,11 @@
 import {
   CalendarIcon,
   ChartPieIcon,
-  DocumentDuplicateIcon,
   FolderIcon,
   HomeIcon,
+  MapPinIcon,
+  TicketIcon,
+  TruckIcon,
   UsersIcon,
 } from '@heroicons/vue/24/outline'
 
@@ -98,11 +100,11 @@ type TeamItem = {
 
 const navigation: NavItem[] = [
   { name: 'Dashboard', href: '#', icon: HomeIcon, count: '5', current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, count: '12', current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, count: '20+', current: false },
-  { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+  { name: 'Usuarios', href: '#', icon: UsersIcon, current: false },
+  { name: 'Vehículos', href: '#', icon: TruckIcon, count: '12', current: false },
+  { name: 'Reservas', href: '#', icon: ChartPieIcon, current: false },
+  { name: 'Geofencing', href: '#', icon: MapPinIcon, count: '20+', current: false },
+  { name: 'Tickets', href: '#', icon: TicketIcon, current: false },
 ]
 
 const teams: TeamItem[] = [
